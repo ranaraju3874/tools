6a10aea64b290f08a80681bd1604eb74db92dedf
ranaraju3874 <138688463+ranaraju3874@users.noreply.github.com>
2023-07-06 19:21:11 +0530

    Update main.py

--- dropbox/transferdata/main.py
+++ dropbox/transferdata/main.py
@@ -4,14 +4,12 @@
 from transferdata import TransferData
 
 def main():
-	access_token = '****'	# get your access token from Dropbox Developers
+	access_token = 'AtomX_Packs_Collection_2023.part10.rar'	# get your access token from Dropbox Developers
 	transferData = TransferData(access_token)
 
-	file_from = 'test.txt'
-	file_to = '/test_dropbox/test.txt'	# The full path to upload the file to, including the file name
+	file_from = 'AtomX_Packs_Collection_2023.part10.rar'
+	file_to = '/AtomX_Packs_Collection_2023.part10.rar'	# The full path to upload the file to, including the file name
 
-	# API v1
-	transferData.upload_file_v1(file_from=file_from, file_to=file_to)
 
 	# API v2
 	transferData.upload_file(file_from=file_from, file_to=file_to)
